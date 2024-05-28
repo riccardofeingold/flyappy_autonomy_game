ae78031a28dd3258e9c927e1807123aca795282b
Riccardo Feingold <riccardofeingold@gmail.com>
2024-05-29 00:16:43 +0200

    allowing also cv::Point2f type for closestPoint method

--- flyappy_autonomy_code/include/flyappy_autonomy_code/utils/maths.hpp
+++ flyappy_autonomy_code/include/flyappy_autonomy_code/utils/maths.hpp
@@ -9,6 +9,7 @@ namespace Maths
 {
     Eigen::Vector2f farthestPoint(const std::vector<Eigen::Vector2f>& vecList);
     Eigen::Vector2f closestPoint(const std::vector<Eigen::Vector2f>& vecList);
+    Eigen::Vector2f closestPoint(const std::vector<cv::Point2f>& vecList);
     
     Eigen::Vector2f mean(const std::vector<cv::Point2f>& points);
 } // namespace Maths
