07321105501c413bd0095125aca915f561ae8e8d
Riccardo Feingold <riccardofeingold@gmail.com>
2024-05-28 19:37:41 +0200

    some code for mpc

--- flyappy_autonomy_code/include/flyappy_autonomy_code/controller/mpc_controller.hpp
+++ flyappy_autonomy_code/include/flyappy_autonomy_code/controller/mpc_controller.hpp
@@ -1,12 +1,25 @@
+#pragma once
+
 #include <qpOASES.hpp>
+
+using namespace qpOASES;
 
 class MPCController 
 {
     public:
-    MPCController();
+    MPCController(
+        int Nx, // state dimension
+        int Nu, // control dimension
+        int N // horizon
+    );
     
-    protected:
+    ~MPCController();
 
     private:
+    /// @brief Construct the H matrix based on the Q and R matrices used in the quadratice cost function
+    /// @param Q
+    /// @param R
+    void setProblemData(const real_t* Q, const real_t* R);
 
+    /// @brief 
 };
--- flyappy_autonomy_code/src/controller/mpc_controller.cpp
+++ flyappy_autonomy_code/src/controller/mpc_controller.cpp
@@ -1,6 +1,1 @@
 #include "flyappy_autonomy_code/controller/mpc_controller.hpp"
-
-MPCController::MPCController()
-{
-    
-}
