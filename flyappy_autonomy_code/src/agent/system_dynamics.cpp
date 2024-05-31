8de3ce77ab0027eb13785a0f796da32b822f7d8c
Riccardo Feingold <riccardofeingold@gmail.com>
2024-05-31 12:51:05 +0200

    Resolved mistake in dyanmics formulation

--- flyappy_autonomy_code/src/agent/system_dynamics.cpp
+++ flyappy_autonomy_code/src/agent/system_dynamics.cpp
@@ -2,36 +2,36 @@
 
 SystemDynamics::SystemDynamics()
 {
-    A << 0, 1, 0, 0,
+     A << 0, 1, 0, 0,
          0, 0, 0, 0,
          0, 0, 0, 1,
          0, 0, 0, 0;
-    
-    Nx = A.rows();
 
-    Ad << 1, SAMPLING_TIME, 0, 0,
-         0, 1, SAMPLING_TIME, 0,
+     Nx = A.rows();
+
+     Ad << 1, SAMPLING_TIME, 0, 0,
+         0, 1, 0, 0,
          0, 0, 1, SAMPLING_TIME,
          0, 0, 0, 1;
 
-    B << 0, 0,
+     B << 0, 0,
          1, 0,
          0, 0,
          0, 1;
     
-    Nu = B.cols();
+     Nu = B.cols();
 
-    Bd << 0.5*SAMPLING_TIME*SAMPLING_TIME, 0,
+     Bd << 0.5*SAMPLING_TIME*SAMPLING_TIME, 0,
           SAMPLING_TIME, 0,
           0, 0.5*SAMPLING_TIME*SAMPLING_TIME,
           0, SAMPLING_TIME;
-    
-    C << 1, 0, 0, 0,
+     
+     C << 1, 0, 0, 0,
          0, 0, 1, 0;
 
-    Ny = C.rows();
+     Ny = C.rows();
 
-    Cd = C;
+     Cd = C;
 }
 
 Eigen::Vector4f SystemDynamics::nextState(const Eigen::Vector2f& Uk, const Eigen::Vector4f& Xk)
